88f44dbce9d4ccaaff61de30923bde986081e9b1
Alex Taylor <alex@alext.xyz>
2018-12-16 18:59:27 -0800

    Bump version

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ fn cli() -> Arguments {
     let env_key = env::var("TUMBLR_API_KEY");
 
     let matches = App::new("tumblr-likes")
-        .version("0.3.0")
+        .version("0.3.1")
         .author("Alex Taylor <alex@alext.xyz>")
         .about("Downloads your liked photos and videos on Tumblr.")
         .arg(
