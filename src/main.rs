351b99f3cf5ea2eb2c1975af098d0a823389866a
Alex Taylor <alex@alext.xyz>
2018-12-16 18:06:58 -0800

    Remove unnecessary Font Awesome

--- src/main.rs
+++ src/main.rs
@@ -246,7 +246,6 @@ static HTML_TEMPLATE: &'static str = "<!DOCTYPE html>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <title>Tumblr Likes</title>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css'>
-    <script defer src='https://use.fontawesome.com/releases/v5.6.1/js/all.js'></script>
     <style>
         .container {
             max-width: 625px;
